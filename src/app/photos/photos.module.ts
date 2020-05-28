cc32330effac0dc7b1bc58fbd3987a8fd28b27a3
Felipe Fortes <felipe.bergmann.f@gmail.com>
2020-05-28 02:09:19 -0300

    Modulo6

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,10 +6,19 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
+import { PhotoList } from './photo-list/photo-list.resolver/photo-list.resolver.component';
 
 @NgModule({
-    declarations:[PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
-    imports:[HttpClientModule, CommonModule],
+    declarations:[  PhotoComponent, 
+                    PhotoListComponent, 
+                    PhotoFormComponent, 
+                    PhotosComponent, 
+                    FilterByDescriptionPipe, PhotoList.ResolverComponent
+                ],
+    imports:[   HttpClientModule, 
+                CommonModule
+            ],
     exports: []
 })
 export class PhotosModule{}
