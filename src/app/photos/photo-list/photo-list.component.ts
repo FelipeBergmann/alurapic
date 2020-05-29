a00e7ce19136051f58bdd614564461e785d729a1
Felipe Fortes <felipe.bergmann.f@gmail.com>
2020-05-29 01:57:38 -0300

    adicionado paginação. Adicionado debounce time no search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -8,15 +11,23 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
     constructor(private photoService: PhotoService, 
                 private activatedRoute: ActivatedRoute){}
 
     ngOnInit(): void {
         this.photos = this.activatedRoute.snapshot.data.photos;
-    }    
+        this.debounce
+              .pipe(debounceTime(300))
+              .subscribe(filter => this.filter = filter);
+    }
+
+    ngOnDestroy(): void {
+      this.debounce.unsubscribe();
+    }
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,14 +7,17 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { PhotoList } from './photo-list/photo-list.resolver/photo-list.resolver.component';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+
+
 
 @NgModule({
     declarations:[  PhotoComponent, 
                     PhotoListComponent, 
                     PhotoFormComponent, 
                     PhotosComponent, 
-                    FilterByDescriptionPipe, PhotoList.ResolverComponent
+                    FilterByDescriptionPipe, LoadButtonComponent
+                    
                 ],
     imports:[   HttpClientModule, 
                 CommonModule
