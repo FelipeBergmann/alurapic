b75c42836f94ae693434210cc1b4e3bdb729bea6
Felipe Fortes <felipe.bergmann.f@gmail.com>
2020-07-01 00:15:17 -0300

    mlayout de mensagem padrão componentizado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
+import { VMessageComponent } from '../shared/components/vMessage/vmessage.component';
+import { VMessageModule } from '../shared/components/vMessage/vmessage.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VMessageModule
   ],
   declarations: [ SignInComponent ]
 })
